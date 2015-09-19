89207113226dec6f9169b75b88a2078601d9563b
Shane <shane8641@live.com>
2015-09-19 18:20:11 -0400

    Added days and years array

--- calendar.php
+++ calendar.php
@@ -19,6 +19,12 @@ $months = array(1 => 'January',
     'May','June','July','August',
     'September','October',
     'November','December');
+// Make the days array
+// 29 in Feb to account for leap year
+$days = array(1 => '31','29','31','30',
+    '31','30','31','31','30','31','30','31');
+// Make years array
+$years = range(2011,2021);
 ?>
 </form>
 </body>
