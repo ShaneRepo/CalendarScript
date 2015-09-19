b31418e2c16f214615ba09fa909828b7d172ad22
Shane <shane8641@live.com>
2015-09-19 18:10:18 -0400

    Added months array

--- calendar.php
+++ calendar.php
@@ -8,11 +8,17 @@
 
 <?php
 /**
- * Calender script
+ * Calendar script
  * Shane Teall
  * 9/19/2015
  */
 
+// Make the Months array
+$months = array(1 => 'January',
+    'February','March','April',
+    'May','June','July','August',
+    'September','October',
+    'November','December');
 ?>
 </form>
 </body>
